6302f6a4341fc849928a4eebc92f4ae6f6c79316
Donna-Sayos <Donnasayos@gmail.com>
2022-12-06 13:30:57 -0500

    updated todo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
-import TodoList from "./components/TodoList";
+import SingleTodo from "./components/SingleTodo";
 
 const LOCAL_STORAGE_KEY = "react-practice-todo-app";
 
 function App() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
-
+  const [visible, setVisible] = useState(true);
 
   const changeHandler = (e) => setText(e.target.value);
 
   const submitHandler = () => {
     if(text === '') return;
-    setTodos([...todos, { todo: text, complete: false }]) 
+    setTodos([...todos, text]) 
     setText("")
   };
 
@@ -21,16 +21,23 @@ function App() {
   const deleteHandler = (todo) =>
     setTodos(todos.filter((item) => item !== todo));
 
+  const toggleVisible = () => {
+    visible === true ? setVisible(false) : setVisible(true);
+  }
+
   return (
-    <>
-      <TodoList todos={todos} deleteHandler={deleteHandler} />
+    <div>
+      <h4 style={{ textAlign: "center" }} >{todos.length} left to do!</h4>
       <input type="text" value={text} onChange={changeHandler} />
-      <button style={{ margin: "5px" }} onClick={submitHandler}>
+      <button style={{ margin: "5px" }} onClick={() => submitHandler()}>
         ADD
       </button>
-      <button onClick={clearHandler}>CLEAR</button>
-      <div> {todos.length} to do left!</div>
-    </>
+      <button onClick={() => clearHandler()}>CLEAR</button>
+      <button style={{ margin: "5px" }} onClick={() => toggleVisible()}>{visible ? "Hide" : "Show"}</button>
+      {visible && todos.map((todo, i) => (
+        <SingleTodo key={i} todo={todo} deleteHandler={deleteHandler} />
+      ))}
+    </div>
   );
 }
 
--- src/components/SingleTodo.jsx
+++ src/components/SingleTodo.jsx
@@ -1,25 +1,11 @@
 import React, {useState} from 'react'
 
 function SingleTodo({ todo, deleteHandler }) {
-    const [check, setCheck] = useState(false);
-
-    const checkHandler = () => (
-        todo.complete = true,
-        setCheck(true)
-    )
-
-    // console.log('checked', check)
-    // console.log('single todo', todo)
+    
   return (
     <div>
-        <label>
-            <input type="checkbox" checked={todo.complete} onChange={checkHandler} />
-            {todo.todo}
-            <button 
-            style={{ margin: "5px" }}
-            onClick={() => deleteHandler(todo)}
-            >x</button>
-        </label>
+        <p style={{ display: "inline" }}>{todo}</p>
+        <button style={{ margin: "5px" }} onClick={() => deleteHandler(todo)}>x</button>
     </div>
   )
 }
