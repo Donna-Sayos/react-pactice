01916be3069c92e90f5cee188b5dc25759330ada
Donna-Sayos <Donnasayos@gmail.com>
2022-12-12 16:17:38 -0500

    added log and fixed css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import IMG from "./assets/logo.png";
 
 import Homepage from "../src/components/homepage/Homepage";
 import About from "../src/components/about/About";
@@ -14,27 +15,41 @@ import ListProducts from "./components/products/ListProducts";
 function App() {
   return (
     <>
-      <nav>
-        <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="about" className="nav-item">About</Link>
-        <Link to="products" className="nav-item">Products</Link>
-        <Link to="login" className="nav-item">Login</Link>
-      </nav>
+      <header className="header">
+        <Link to='/' className="logo">
+          <img src={IMG} alt='logo' />
+        </Link>
+        <nav>
+          <Link to="/" className="nav-item">
+            Homepage
+          </Link>
+          <Link to="about" className="nav-item">
+            About
+          </Link>
+          <Link to="products" className="nav-item">
+            Products
+          </Link>
+          <Link to="login" className="nav-item">
+            Login
+          </Link>
+        </nav>
+      </header>
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
-        <Route path="error" element={<ErrorPage />} />
-
         <Route path="products" element={<Products />}>
           <Route path="search" element={<Search />} />
           <Route path="list" element={<ListProducts />} />
           <Route path="add" element={<AddProducts />} />
           <Route path=":id" element={<ProductDisplay />} />
         </Route>
+        <Route path="error" element={<ErrorPage />}>
+          <Route path="list" element={<ListProducts />} />
+        </Route>
       </Routes>
     </>
-  )
-};
+  );
+}
 
 export default App;
--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import IMG from "../assets/404-error.gif";
 
 export default function ErrorPage() {
   const navigate = useNavigate();
 
   return (
-    <div>
-        <img src={IMG} alt='error img' />
-        <button onClick={() => navigate('/products')}>back</button>
+    <div className="error">
+      <button onClick={() => navigate("/products/list")}>back</button>
+      <img src={IMG} alt="error img" />
     </div>
-  )
-};
+  );
+}
