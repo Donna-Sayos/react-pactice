5aa561e56b55b568edbfb7360c1cb949d879048b
Donna-Sayos <Donnasayos@gmail.com>
2022-12-30 12:46:16 -0500

    added React lazy successfully

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import { lazy, Suspense } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import IMG from "./assets/logo.png";
 
-import Homepage from "../src/components/homepage/Homepage";
-import About from "../src/components/about/About";
-import Login from "../src/components/login/Login";
-import ErrorPage from "../src/components/fallback/ErrorPage";
+// import Homepage from "../src/components/homepage/Homepage";
+// import About from "../src/components/about/About";
+// import Login from "../src/components/login/Login";
 
-import Products from "../src/components/products/Products";
-import AddProducts from "../src/components/products/AddProducts";
-import Search from "../src/components/products/Search";
-import ProductDisplay from "../src/components/products/ProductDisplay";
-import ListProducts from "./components/products/ListProducts";
+import ErrorPage from "../src/components/fallback/ErrorPage";
+import Loading from "../src/components/fallback/Loading";
+
+// import Products from "../src/components/products/Products";
+// import AddProducts from "../src/components/products/AddProducts";
+// import Search from "../src/components/products/Search";
+// import ProductDisplay from "../src/components/products/ProductDisplay";
+// import ListProducts from "./components/products/ListProducts";
+
+const Homepage = lazy(() => import("../src/components/homepage/Homepage"));
+const About = lazy(() => import("../src/components/about/About"));
+const Login = lazy(() => import("../src/components/login/Login"));
+
+const Products = lazy(() => import("../src/components/products/Products"));
+const AddProducts = lazy(() =>
+  import("../src/components/products/AddProducts")
+);
+const Search = lazy(() => import("../src/components/products/Search"));
+const ProductDisplay = lazy(() =>
+  import("../src/components/products/ProductDisplay")
+);
+const ListProducts = lazy(() => import("./components/products/ListProducts"));
 
 function App() {
   return (
     <>
       <header className="header">
-        <Link to='/' className="logo">
-          <img src={IMG} alt='logo' />
+        <Link to="/" className="logo">
+          <img src={IMG} alt="logo" />
         </Link>
         <nav>
           <Link to="/" className="nav-item">
@@ -36,22 +52,24 @@ function App() {
           </Link>
         </nav>
       </header>
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="about" element={<About />} />
-        <Route path="login" element={<Login />} />
-        <Route path="products" element={<Products />}>
-          <Route path="search" element={<Search />} />
-          <Route path="list" element={<ListProducts />} />
-          <Route path="add" element={<AddProducts />} />
-          <Route path=":id" element={<ProductDisplay />} />
-        </Route>
-        <Route path="error" element={<ErrorPage />}>
-          <Route path="list" element={<ListProducts />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+          <Route path="products" element={<Products />}>
+            <Route path="search" element={<Search />} />
+            <Route path="list" element={<ListProducts />} />
+            <Route path="add" element={<AddProducts />} />
+            <Route path=":id" element={<ProductDisplay />} />
+          </Route>
+          <Route path="error" element={<ErrorPage />}>
+            <Route path="list" element={<ListProducts />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
-  )
-};
+  );
+}
 
 export default App;
