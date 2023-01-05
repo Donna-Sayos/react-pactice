da3a0d1c5ad0aa6ec2c71e38e5aa95c5ceb15284
Donna-Sayos <Donnasayos@gmail.com>
2023-01-04 23:42:59 -0500

    edited addproducts

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -5,7 +5,7 @@ export default function Products() {
   return (
     <div className='products'>
       <h1 className='mt-5 p-2'>Hello from products</h1>
-      <div className='productsNav mt-5 p-4'>
+      <div className='productsNav mt-5 p-5 text-center'>
         <Link to="/products/search">Search</Link>
         <Link to="/products/list">List</Link>
         <Link to="/products/add">Add</Link>
