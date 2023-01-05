5d6c9e88fb9bc51ab1ce676284b31087c280c460
Donna-Sayos <Donnasayos@gmail.com>
2023-01-04 23:36:56 -0500

    edited the about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className="mt-5 p-5" style={{ position: "absolute" }}>
-      <h1 className="mt-5 px-5">Hello from About..</h1>
+    <div className="mt-5 p-5 text-center" style={{ display: "inline" }}>
+      <h1 className="mt-5 p-5">Hello from About..</h1>
     </div>
   )
 };
