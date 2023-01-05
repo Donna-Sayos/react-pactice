63416a21306bebf8d81e566741ab96d0a5be43e5
Donna-Sayos <Donnasayos@gmail.com>
2023-01-04 23:35:01 -0500

    finished styling the mouse logger component

--- src/components/homepage/PanelMouseLogger.js
+++ src/components/homepage/PanelMouseLogger.js
@@ -1,19 +1,25 @@
-import "./index.css"
+import "./index.css";
 import MousePosition from "./MousePosition";
 
 export default function PanelMouseLogger() {
   return (
-    <div>
-      <h2 >Mouse Position</h2>
-      <br />
-      <MousePosition
-        render={({ mousePosition }) => (
-          <div>
-            <p>X: {mousePosition.x}</p>
-            <p>Y: {mousePosition.y}</p>
-          </div>
-        )}
-      />
+    <div className="logger-container container-fluid card">
+      <div className="p-4">
+        <h2>Mouse Position</h2>
+        <br />
+        <MousePosition
+          render={({ mousePosition }) => (
+            <div>
+              <b>
+                <p>X: <span>{mousePosition.x}</span></p>
+              </b>
+              <b>
+                <p>Y: <span>{mousePosition.y}</span></p>
+              </b>
+            </div>
+          )}
+        />
+      </div>
     </div>
   );
 }
